6db3863c765acf125109b2590be244e0346d2c3f
Sandeep Ray <sandy.dgr4@gmail.com>
2025-07-26 14:04:21 +0530

    Update lcd_core.h

--- lcd_core.h
+++ lcd_core.h
@@ -1,15 +1,14 @@
 #ifndef LCD_CORE_H
 #define LCD_CORE_H
 
-#include <avr/io.h>
-#include <util/delay.h>
+#include <stdint.h>
 
 void lcd_init(void);
-void lcd_cmd(uint8_t cmd);
-void lcd_data(uint8_t data);
 void lcd_clear(void);
 void lcd_set_cursor(uint8_t row, uint8_t col);
 void lcd_print(const char *str);
+void lcd_cmd(uint8_t cmd);
+void lcd_data(uint8_t data);
 void lcd_create_char(uint8_t location, uint8_t charmap[]);
 
 #endif
