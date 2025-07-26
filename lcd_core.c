4148c5656eb469c2924f73b51d7f0f0c144fa5a4
Sandeep Ray <sandy.dgr4@gmail.com>
2025-07-26 14:03:32 +0530

    Update lcd_core.c

--- lcd_core.c
+++ lcd_core.c
@@ -1,60 +1,54 @@
 #include "lcd_config.h"
-#include "lcd_core.h"
+#include "lcd_lowlevel.h"
 
 static void lcd_pulse_enable() {
-    LCD_EN_PORT |= (1 << LCD_EN_PIN);
-    _delay_us(1);
-    LCD_EN_PORT &= ~(1 << LCD_EN_PIN);
-    _delay_us(100);
+    LCD_EN_WRITE(1);
+    LCD_DELAY_US(1);
+    LCD_EN_WRITE(0);
+    LCD_DELAY_US(100);
 }
 
 static void lcd_write_4bits(uint8_t data) {
-    if (data & 0x01) LCD_D4_PORT |= (1 << LCD_D4_PIN); else LCD_D4_PORT &= ~(1 << LCD_D4_PIN);
-    if (data & 0x02) LCD_D5_PORT |= (1 << LCD_D5_PIN); else LCD_D5_PORT &= ~(1 << LCD_D5_PIN);
-    if (data & 0x04) LCD_D6_PORT |= (1 << LCD_D6_PIN); else LCD_D6_PORT &= ~(1 << LCD_D6_PIN);
-    if (data & 0x08) LCD_D7_PORT |= (1 << LCD_D7_PIN); else LCD_D7_PORT &= ~(1 << LCD_D7_PIN);
+    LCD_D4_WRITE((data >> 0) & 0x01);
+    LCD_D5_WRITE((data >> 1) & 0x01);
+    LCD_D6_WRITE((data >> 2) & 0x01);
+    LCD_D7_WRITE((data >> 3) & 0x01);
     lcd_pulse_enable();
 }
 
 void lcd_cmd(uint8_t cmd) {
-    LCD_RS_PORT &= ~(1 << LCD_RS_PIN); // RS = 0
+    LCD_RS_WRITE(0); // Command mode
     lcd_write_4bits(cmd >> 4);
     lcd_write_4bits(cmd & 0x0F);
-    _delay_ms(2);
+    LCD_DELAY_MS(2);
 }
 
 void lcd_data(uint8_t data) {
-    LCD_RS_PORT |= (1 << LCD_RS_PIN); // RS = 1
+    LCD_RS_WRITE(1); // Data mode
     lcd_write_4bits(data >> 4);
     lcd_write_4bits(data & 0x0F);
-    _delay_ms(2);
+    LCD_DELAY_MS(2);
 }
 
 void lcd_init() {
-    LCD_RS_DDR |= (1 << LCD_RS_PIN);
-    LCD_EN_DDR |= (1 << LCD_EN_PIN);
-    LCD_D4_DDR |= (1 << LCD_D4_PIN);
-    LCD_D5_DDR |= (1 << LCD_D5_PIN);
-    LCD_D6_DDR |= (1 << LCD_D6_PIN);
-    LCD_D7_DDR |= (1 << LCD_D7_PIN);
+    DDRB |= 0x3F; // Set PORTB0-5 as output (or use macros if you want per pin DDR)
 
-    _delay_ms(40);
-
-    lcd_write_4bits(0x03); _delay_ms(5);
-    lcd_write_4bits(0x03); _delay_us(150);
-    lcd_write_4bits(0x03); _delay_us(150);
+    LCD_DELAY_MS(40);
+    lcd_write_4bits(0x03); LCD_DELAY_MS(5);
+    lcd_write_4bits(0x03); LCD_DELAY_US(150);
+    lcd_write_4bits(0x03); LCD_DELAY_US(150);
     lcd_write_4bits(0x02); // Set 4-bit mode
 
     lcd_cmd(0x28); // 4-bit, 2 line, 5x8 font
     lcd_cmd(0x0C); // Display ON
     lcd_cmd(0x06); // Entry mode
     lcd_cmd(0x01); // Clear
-    _delay_ms(2);
+    LCD_DELAY_MS(2);
 }
 
 void lcd_clear() {
     lcd_cmd(0x01);
-    _delay_ms(2);
+    LCD_DELAY_MS(2);
 }
 
 void lcd_set_cursor(uint8_t row, uint8_t col) {
